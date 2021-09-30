a58a61b6b6940e42d0384aa155f39d7ad731bde1
HHK <hhk.contact@protonmail.com>
2021-09-30 17:09:02 +0200

    fix: remove useless ISushiBar  interface

--- src/interfaces/ISushiBar.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface ISushiBar is IERC20 {
-    function leave(uint256 _share) external;
-    function enter(uint256 _amount) external;
-}
