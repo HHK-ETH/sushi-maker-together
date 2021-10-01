2a6bc2c47706210852cd7f6dbc16d63b8b1322e8
HHK <hhk.contact@protonmail.com>
2021-10-01 17:14:53 +0200

    feat: add claim tests

--- src/test/SushiMakerTogether.t.sol
+++ src/test/SushiMakerTogether.t.sol
@@ -29,11 +29,12 @@ contract SushiMakerTogetherTest is DSTest {
         //deploy SushiMakerTogether & users
         sushiMakerTogether = new SushiMakerTogether(sushi, sushiBar, address(this));
         multisig = new User(sushi, sushiBar, sushiMakerTogether);
+        userA = new User(sushi, sushiBar, sushiMakerTogether);
 
         //transfer ownership to multisig user once deployed
         sushiMakerTogether.transferOwnership(address(multisig));
 
-        //give sushi uint16.max tokens to users
+        //give sushi uint16.max tokens to multisig
         sushi.mint(address(multisig), 65535 * 10**18);
     }
 
@@ -115,6 +116,36 @@ contract SushiMakerTogetherTest is DSTest {
     
     function testFail_update_lockedOnServing_not_owner() public {
         sushiMakerTogether.updateLockedOnServing(20);
+    }
+
+    function test_claim_no_profits() public {
+        //pre balances
+        uint256 preBalance = sushiMakerTogether.balanceOf(address(multisig));
+        uint256 preTotalSushi = sushiMakerTogether.totalSushi();
+        //claim
+        multisig.claim(address(multisig));
+        //asserts
+        assertEq(sushiMakerTogether.balanceOf(address(multisig)), preBalance);
+        assertEq(sushiMakerTogether.totalSushi(), preTotalSushi);
+    }
+
+    function test_claim_profits(uint16 _amount) public {
+        uint256 amount = uint256(_amount) * 10**18; //convert decimals
+        //pre balances
+        uint256 preBalanceMultisig = sushiMakerTogether.balanceOf(address(multisig));
+        uint256 preBalanceUserA = sushiMakerTogether.balanceOf(address(userA));
+        uint256 preTotalSushi = sushiMakerTogether.totalSushi();
+        //serve the bar
+        sushi.mint(address(sushiBar), amount);
+        //userA claim
+        sushiMakerTogether.claim(address(userA));
+        //post balances
+        uint256 postTotalSushi = sushiMakerTogether.xSushiToSushi(sushiBar.balanceOf(address(sushiMakerTogether)));
+        uint256 profits = postTotalSushi - preTotalSushi;
+        //asserts
+        assertEq(preTotalSushi + profits, postTotalSushi);
+        assertEq(preBalanceMultisig + (profits * sushiMakerTogether.LOCKED_ON_SERVING() / 100), sushiMakerTogether.balanceOf(address(multisig)));
+        assertEq(preBalanceMultisig + (profits * (100 - sushiMakerTogether.LOCKED_ON_SERVING()) / 100), sushiMakerTogether.balanceOf(address(userA)));
     }
 
 }
